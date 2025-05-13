c19c7d499431acc9458f7a73f1a701b99c8b494f
Furkan Kuyumcu <fkuyumcu@k1m15s05.42istanbul.com.tr>
2025-05-13 15:27:32 +0300

    a

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -2,3 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Contact{
+
+    public:
+        Contact();
+        ~Contact();
+};
+
+class PhoneBook{
+
+    Contact c1, c2, c3, c4, c5, c6, c7, c8;
+    public:
+        PhoneBook();
+        ~PhoneBook();
+
+};
