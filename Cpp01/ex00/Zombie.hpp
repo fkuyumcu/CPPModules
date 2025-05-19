151cc0fdba8c7507bd891691430bee89b9473a69
kazanuka <furkankuyumcu2004@gmail.com>
2025-05-19 17:41:22 +0300

    a

---
+++ Cpp01/ex00/Zombie.hpp
@@ -0,0 +1,32 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class  Zombie
+{
+    private:
+        std::string name;
+        
+    public:
+    void announce(void);
+    
+    //Stack and Heap Functions
+    Zombie* newZombie(std::string name);
+    void randomChump(std::string name);
+    
+    //Setters and Getters
+    void setName(std::string name);
+    std::string getName();
+
+    //Constructor and Destructor
+    Zombie(std::string name);
+    ~Zombie();
+};
+
+
+
+
+
+#endif
