df03335b9adc332ea697a954f20fc9014a83a5e5
Furkan Kuyumcu <fkuyumcu@k1m35s10.42istanbul.com.tr>
2025-07-03 17:11:20 +0300

    a

--- cpp01/ex04/Sed.cpp
+++ cpp01/ex04/Sed.cpp
@@ -10,6 +10,12 @@ std::string ft_replace(std::string content, std::string s1, std::string s2)
     size_t pos = 0;
     size_t found_char = 0;
 
+    if(content.find(s1) == std::string::npos)
+    {
+        std::cerr << "No matches found for \"" << s1 << "\"" << std::endl;
+        return content;
+    }
+
     while((found_char = content.find(s1,pos)) != std::string::npos)
     {
         res += content.substr(pos, found_char - pos);
