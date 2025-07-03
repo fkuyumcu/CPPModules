597121707d9cab31809650a520d32465b54d1aad
Furkan Kuyumcu <fkuyumcu@k1m35s10.42istanbul.com.tr>
2025-07-03 16:53:57 +0300

    a

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,20 +1,19 @@
 #include "Sed.hpp"
 
-
 int main(int ac, char **av)
 {
     
     if(ac != 4 || std::string(av[1]).empty() || std::string(av[2]).empty() || std::string(av[3]).empty())
     {
         std::cerr << "Error";
-        exit(1);
+        return(1);
     }
     std::ifstream infile(av[1]);
     
     if(!infile.is_open())
     {
         std::cerr << "Error Opening File";
-        exit(1);
+        return(1);
     }
 
     std::stringstream buffer;
@@ -23,11 +22,11 @@ int main(int ac, char **av)
 
     infile.close();
     
-    std::ofstream outfile(std::string(av[1]) + ".replace");
+    std::ofstream outfile((std::string(av[1]) + ".replace").c_str());
     if(!outfile.is_open())
     {
         std::cerr << "Error Creating File";
-        exit(1);
+        return(1);
     }
     outfile << ft_replace(content, av[2], av[3]);
     outfile.close();
