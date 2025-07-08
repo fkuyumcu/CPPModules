b0a7d3ec41176ca98c984a96a0f3af678a4c8631
Furkan  Kuyumcu <fkuyumcu@Furkan-MacBook-Air.local>
2025-07-08 17:37:39 +0300

    sed

--- cpp01/ex04/Sed.cpp
+++ cpp01/ex04/Sed.cpp
@@ -16,12 +16,12 @@ std::string ft_replace(std::string content, std::string s1, std::string s2)
         return content;
     }
 
-    while((found_char = content.find(s1,pos)) != std::string::npos)//search loop
+    while((found_char = content.find(s1,pos)) != std::string::npos)
     {
-        result += content.substr(pos, found_char - pos);//add part before s1
-        result += s2;//add s2 to result isntead of s1
-        pos = found_char + s1.length(); //position is changed from start to end of s2
+        result += content.substr(pos, found_char - pos);
+        result += s2;
+        pos = found_char + s1.length();
     }
-    result += content.substr(pos);//add part after the last find element
+    result += content.substr(pos);
     return (result);
 }
