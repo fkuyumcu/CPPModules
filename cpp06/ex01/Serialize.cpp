db082ec361ea8b5dc8e340fc044e00e9f18203b2
Furkan  Kuyumcu <furkankuyumcu2004@gmail.com>
2026-02-22 02:23:42 +0300

    ex01 02 edited

--- cpp06/ex01/Serialize.cpp
+++ cpp06/ex01/Serialize.cpp
@@ -1,9 +1,11 @@
 #include "Serialize.hpp"
 
-uintptr_t Serializer::serialize(Data* ptr) {
+uintptr_t Serializer::serialize(Data* ptr)
+{
     return reinterpret_cast<uintptr_t>(ptr);
 }
 
-Data* Serializer::deserialize(uintptr_t raw) {
+Data* Serializer::deserialize(uintptr_t raw)
+{
     return reinterpret_cast<Data*>(raw);
 }
