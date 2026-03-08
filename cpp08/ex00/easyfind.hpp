e29e42dca34049375c226e5d585a214c9b76e32c
Furkan  Kuyumcu <furkankuyumcu2004@gmail.com>
2026-03-08 16:08:29 +0300

    fixed

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -9,10 +9,9 @@ template <typename T>//easyfind type'a göre bir iterator döndürüyor.
 typename T::iterator easyfind(T &container, int value)
 {
     typename T::iterator it = std::find(container.begin(), container.end(), value);
-
+    if (it == container.end())
+        throw std::runtime_error("Element not found in container");
+    return it;
 }
-
-
-
 
 #endif
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -5,7 +5,6 @@
 
 int main()
 {
-    // --- subject testi ---
     Span sp(5);
     sp.addNumber(6);
     sp.addNumber(3);
@@ -15,14 +14,12 @@ int main()
     std::cout << sp.shortestSpan() << std::endl;  // 2
     std::cout << sp.longestSpan() << std::endl;   // 14
 
-    // --- dolu span ---
     try {
         sp.addNumber(99);
     } catch (std::exception &e) {
         std::cout << "Hata: " << e.what() << std::endl;
     }
 
-    // --- tek eleman ---
     Span sp2(1);
     sp2.addNumber(42);
     try {
@@ -31,7 +28,6 @@ int main()
         std::cout << "Hata: " << e.what() << std::endl;
     }
 
-    // --- vector iterator ile toplu ekleme ---
     Span big(10000);
     std::vector<int> src;
     for (int i = 0; i < 10000; i++)
@@ -40,7 +36,6 @@ int main()
     std::cout << big.shortestSpan() << std::endl;  // 1
     std::cout << big.longestSpan() << std::endl;   // 9999
 
-    // --- list iterator ile toplu ekleme ---
     Span sp3(5);
     std::list<int> lst;
     lst.push_back(10);
